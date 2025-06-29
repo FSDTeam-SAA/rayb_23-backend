0d0090bb41db4ed24bda3f2c64f7004f04fda6bb
Sajjad177 <sajjadhossainx06@gmail.com>
2025-06-29 15:52:32 +0600

    feat: add getMyClaimBussiness functionality; implement service, controller methods, and update router

--- src/modules/claimBussiness/claimBussiness.controller.js
+++ src/modules/claimBussiness/claimBussiness.controller.js
@@ -46,10 +46,29 @@ const getAllClaimBussiness = async (req, res) => {
   }
 };
 
+const getMyClaimBussiness = async (req, res) => {
+  try {
+    const { email } = req.user;
+    const result = await claimBussinessService.getMyClaimBussiness(email);
+
+    return res.status(200).json({
+      success: true,
+      code: 200,
+      message: "Claim bussiness retrieved successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, code: 400, message: error.message });
+  }
+};
+
 const claimBussinessController = {
   verifyPhoneNumber,
   documentVerification,
   getAllClaimBussiness,
+  getMyClaimBussiness,
 };
 
 module.exports = claimBussinessController;
