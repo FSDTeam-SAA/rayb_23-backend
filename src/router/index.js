c5c5e9483de639351229955b32a2e9b21ed72c6e
Ahmedturaj <ahmedturaj12@gmail.com>
2025-06-30 13:56:55 +0600

    added saved business route

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ const userRouter = require("../modules/user/user.router");
 const authRouter = require("../modules/auth/auth.router");
 const sendMessageRouter = require("../modules/contract/contract.router");
 const businessRouter = require("../modules/business/business.router");
+const savedBusinessRouter = require("../modules/savedBusiness/savedBusiness.router");
 const router = Router();
 
 const moduleRouter = [
@@ -21,6 +22,10 @@ const moduleRouter = [
   {
     path: "/business",
     router:businessRouter,
+  },
+  {
+    path: "/saved-business",
+    router: savedBusinessRouter,
   }
 ];
 
