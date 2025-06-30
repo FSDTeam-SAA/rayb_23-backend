b7114ad203496856afe25d008bb874526ef74621
Ahmedturaj <ahmedturaj12@gmail.com>
2025-06-30 14:14:00 +0600

    added saved business by user

--- src/modules/savedBusiness/savedBusiness.router.js
+++ src/modules/savedBusiness/savedBusiness.router.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { createSavedBusiness } = require("./savedBusiness.controller");
+const { createSavedBusiness, getSavedBusinessesByUser } = require("./savedBusiness.controller");
 const auth = require("../../middleware/auth");
 const USER_ROLE = require("../user/user.constant");
 const router = express.Router();
 
-router.post("/create",auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), createSavedBusiness);
+router.post("/create",auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), createSavedBusiness)
+// get saved business by user
+router.get("/my-saved-business", auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), getSavedBusinessesByUser)
 
 const savedBusinessRouter = router;
 module.exports = savedBusinessRouter;
