6b9f6286cb4c05f4d11bb9566f2d0125c444c424
Ahmedturaj <ahmedturaj12@gmail.com>
2025-07-07 12:24:46 +0600

    added get all review by admin

--- src/modules/review/review.router.js
+++ src/modules/review/review.router.js
@@ -1,5 +1,5 @@
 const express = require ("express");
-const { createReview } = require("./review.controller");
+const { createReview, getReviewsByAdmin } = require("./review.controller");
 const { upload } = require("../../utils/cloudnary");
 const auth = require("../../middleware/auth");
 const USER_ROLE = require("../user/user.constant");
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post ("/create",  upload.array("image", 5), // 🔹 Accept up to 5 images with field name "image"
 
   auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), createReview);
+
+  router.get("/", getReviewsByAdmin);
 
   const reviewRouter=router;
   module.exports=reviewRouter
