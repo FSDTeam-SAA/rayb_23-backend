543e052d91fd2426de3a53a5dd48366577fa9c1e
Sajjad177 <sajjadhossainx06@gmail.com>
2025-07-12 11:19:50 +0600

    Refactor app and chat router; remove commented-out code and make Socket.IO available via app

--- src/modules/chat/chat.router.js
+++ src/modules/chat/chat.router.js
@@ -1,25 +1,20 @@
 const { Router } = require("express");
 const chatController = require("./chat.controller");
-const auth = require("../../middleware/auth");
-const USER_ROLE = require("../user/user.constant");
 
 const router = Router();
 
 router.post(
   "/create",
-  // auth(USER_ROLE.bussinessMan, USER_ROLE.user, USER_ROLE.admin),
   chatController.createChat
 );
 
 router.get(
   "/",
-  // auth(USER_ROLE.bussinessMan, USER_ROLE.user, USER_ROLE.admin),
   chatController.getChat
 );
 
 router.get(
   "/my-chat/:userId",
-  // auth(USER_ROLE.bussinessMan, USER_ROLE.user, USER_ROLE.admin),
   chatController.getMyChat
 );
 
