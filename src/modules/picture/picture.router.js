c9f31659a60c84c2783a7663677173bb891a5dcb
Ahmedturaj <ahmedturaj12@gmail.com>
2025-07-14 15:07:36 +0600

    added picture router

--- src/modules/picture/picture.router.js
+++ src/modules/picture/picture.router.js
@@ -1,11 +1,17 @@
 const express = require ("express");
 const auth = require("../../middleware/auth");
 const USER_ROLE = require("../user/user.constant");
-const { uploadPicture } = require("./picture.controller");
+const { uploadPicture, getAllPicturesAdmin, getAllPicturesByUser, getPictureById, updatePictureById, deletedPicture, getPictureByBusinessId } = require("./picture.controller");
 const { upload } = require("../../utils/cloudnary");
 const router = express.Router();
 
-router.post("/upload-image", auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user),upload.array("image", 5), uploadPicture);
+router.post("/upload-image", auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user),upload.array("image", 5), uploadPicture)
+router.get("/get-all-pictures", auth(USER_ROLE.admin), getAllPicturesAdmin);
+router.get("/get-all-pictures-by-business/:id", auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), getPictureByBusinessId);
+router.get("/get-all-pictures-by-user", auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), getAllPicturesByUser);
+router.get("/get-picture-by-id/:id", auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), getPictureById);
+router.put("/update-picture/:id", auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), upload.array("image", 5), updatePictureById);
+router.delete("/delete-picture/:id", auth(USER_ROLE.admin, USER_ROLE.bussinessMan, USER_ROLE.user), deletedPicture);
 
 const pictureRouter = router;
 module.exports= pictureRouter;
