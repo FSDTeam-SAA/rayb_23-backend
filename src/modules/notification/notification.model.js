495fc7472930e45c5befb7b5c2b5ca244a5532b3
Sajjad177 <sajjadhossainx06@gmail.com>
2025-07-16 18:19:17 +0600

    user details added

--- src/modules/notification/notification.model.js
+++ src/modules/notification/notification.model.js
@@ -1,28 +1,28 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
-const NotifySchema = new mongoose.Schema({
-       user: {
-           type: mongoose.Schema.Types.ObjectId,
-           ref: "User",
-           required: true,
-       },
-       text:{
-        type:String
-       },
-       type:{
-        type:String
-       },
-       read:{
-        type:Boolean,
-        default:false
-       },
-    
+const NotifySchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    {
-        timestamps:true
-    }
+    text: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const NotifyModel= mongoose.model("Notification", NotifySchema);
+const NotifyModel = mongoose.model("Notification", NotifySchema);
 
-module.exports= NotifyModel;
+module.exports = NotifyModel;
