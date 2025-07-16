0515a404580a69c90a8486d524bc40c6d240b319
Sajjad177 <sajjadhossainx06@gmail.com>
2025-07-16 16:06:08 +0600

    notification route setup completed

--- src/modules/notification/notification.router.js
+++ src/modules/notification/notification.router.js
@@ -1,7 +1,14 @@
-const express = require ("express");
+const express = require("express");
 const { getAllNotification } = require("./notification.controller");
 const auth = require("../../middleware/auth");
 const USER_ROLE = require("../user/user.constant");
 const router = express.Router();
 
-router.get("/all-notify",auth(USER_ROLE.admin, USER_ROLE.businessMan, USER_ROLE.user), getAllNotification)
+router.get(
+  "/all-notify",
+  auth(USER_ROLE.admin, USER_ROLE.businessMan, USER_ROLE.user),
+  getAllNotification
+);
+
+const notificationRouter = router;
+module.exports = notificationRouter;
