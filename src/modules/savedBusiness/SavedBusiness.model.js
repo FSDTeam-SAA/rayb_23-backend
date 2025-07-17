0fbfa5d9b259d91e0a52e509fbad1fa95028e0b3
Sajjad177 <sajjadhossainx06@gmail.com>
2025-07-17 17:10:16 +0600

    status and error is updated

--- src/modules/savedBusiness/SavedBusiness.model.js
+++ src/modules/savedBusiness/SavedBusiness.model.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
-const savedBusinessSchema = new mongoose.Schema({
+const savedBusinessSchema = new mongoose.Schema(
+  {
     savedBusiness: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Business",
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Business",
+      required: true,
     },
-   
+
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    }
-}, {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
 const SavedBusinessModel = mongoose.model("SavedBusiness", savedBusinessSchema);
 module.exports = SavedBusinessModel;
--- src/modules/savedBusiness/savedBusiness.controller.js
+++ src/modules/savedBusiness/savedBusiness.controller.js
@@ -1,59 +1,60 @@
 const SavedBusinessModel = require("./SavedBusiness.model");
 const User = require("../user/user.model");
 
-
 // Create Saved Business
 exports.createSavedBusiness = async (req, res) => {
-    try {
-        const { savedBusiness } = req.body;
-        
-        const userId = req.user.userId;
-        if (!userId) {
-            return res.status(401).json({ message: "Unauthorized user" });
-        }
+  try {
+    const { savedBusiness } = req.body;
 
-        if (!savedBusiness) {
-            return res.status(400).json({ message: "Business ID is required" });
-        }
-
-        const userExists = await User.findById(userId);
-        if (!userExists) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        const alreadySaved = await SavedBusinessModel.findOne({
-            savedBusiness: savedBusiness,
-            user: userId
-        });
-
-        if (alreadySaved) {
-            return res.status(400).json({ message: "Business already saved" });
-        }
-
-        const newSaved = new SavedBusinessModel({
-            savedBusiness: savedBusiness,
-            user: userId
-        });
-
-        const savedData = await newSaved.save();
-
-        return res.status(201).json({
-            message: "Business saved successfully",
-            data: savedData
-        });
-
-    } catch (error) {
-        console.error("Error in createSavedBusiness:", error);
-        res.status(500).json({ message: "Internal server error", error: error.message });
+    const userId = req.user.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("User not found");
     }
+
+    if (!savedBusiness) {
+      return res.status(400).json({ message: "Business ID is required" });
+    }
+
+    // const userExists = await User.findById(userId);
+    // if (!userExists) {
+    //   return res.status(404).json({ message: "User not found" });
+    // }
+
+    const alreadySaved = await SavedBusinessModel.findOne({
+      savedBusiness: savedBusiness,
+      user: userId,
+    });
+
+    if (alreadySaved) {
+      throw new Error("Business already saved");
+    }
+
+    const newSaved = new SavedBusinessModel({
+      savedBusiness: savedBusiness,
+      user: userId,
+    });
+
+    const savedData = await newSaved.save();
+
+    return res.status(201).json({
+      message: "Business saved successfully",
+      data: savedData,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ code: 500, status: false, message: error.message });
+  }
 };
 
 // get saved businesses by user
 exports.getSavedBusinessesByUser = async (req, res) => {
   try {
     const userId = req.user.userId;
-    if (!userId) {
-      return res.status(401).json({ message: "Unauthorized user" });
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("User not found");
     }
 
     const savedBusinesses = await SavedBusinessModel.find({ user: userId })
@@ -65,12 +66,12 @@ exports.getSavedBusinessesByUser = async (req, res) => {
           {
             path: "review",
             match: { status: "approved" },
-            populate: { path: "user", select: "name email" }
+            populate: { path: "user", select: "name email" },
           },
-          { path: "user", select: "name email" }
-        ]
+          { path: "user", select: "name email" },
+        ],
       })
-      .populate("user", "name email")
+      .populate("user", "name email");
 
     if (savedBusinesses.length === 0) {
       return res.status(200).json({ message: "No saved businesses found" });
@@ -79,26 +80,26 @@ exports.getSavedBusinessesByUser = async (req, res) => {
     return res.status(200).json({
       status: true,
       message: "Saved businesses fetched successfully",
-      data: savedBusinesses
+      data: savedBusinesses,
     });
-
   } catch (error) {
     console.error("Error in getSavedBusinessesByUser:", error);
-    res.status(500).json({ message: "Internal server error", error: error.message });
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
 
-
 //get saved business by id
 exports.getSavedBusinessById = async (req, res) => {
-    try {
-        const { id } = req.params;
-        if (!id) {
-            return res.status(400).json({ message: "Business ID is required" });
-        }
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "Business ID is required" });
+    }
 
-        const savedBusiness = await SavedBusinessModel.findById(id)
-            .populate({
+    const savedBusiness = await SavedBusinessModel.findById(id)
+      .populate({
         path: "savedBusiness",
         populate: [
           { path: "instrumentInfo" },
@@ -106,50 +107,52 @@ exports.getSavedBusinessById = async (req, res) => {
           {
             path: "review",
             match: { status: "approved" },
-            populate: { path: "user", select: "name email" }
+            populate: { path: "user", select: "name email" },
           },
-          { path: "user", select: "name email" }
-        ]
+          { path: "user", select: "name email" },
+        ],
       })
-            .populate("user", "name email");
+      .populate("user", "name email");
 
-        if (!savedBusiness) {
-            return res.status(404).json({ message: "Saved business not found" });
-        }
-
-        return res.status(200).json({
-            status: true,
-            message: "Saved business fetched successfully",
-            data: savedBusiness
-        });
-
-    } catch (error) {
-        console.error("Error in getSavedBusinessById:", error);
-        res.status(500).json({ message: "Internal server error", error: error.message });
+    if (!savedBusiness) {
+      return res.status(404).json({ message: "Saved business not found" });
     }
-}
+
+    return res.status(200).json({
+      status: true,
+      message: "Saved business fetched successfully",
+      data: savedBusiness,
+    });
+  } catch (error) {
+    console.error("Error in getSavedBusinessById:", error);
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
 
 // delete saved business by id
 exports.deleteSavedBusiness = async (req, res) => {
-    try {
-        const { id } = req.params;
-        if (!id) {
-            return res.status(400).json({ message: "Business ID is required" });
-        }
-
-        const deletedBusiness = await SavedBusinessModel.findByIdAndDelete(id);
-        if (!deletedBusiness) {
-            return res.status(404).json({ message: "Saved business not found" });
-        }
-
-        return res.status(200).json({
-            status: true,
-            message: "Saved business deleted successfully",
-            data: deletedBusiness
-        });
-
-    } catch (error) {
-        console.error("Error in deleteSavedBusinessById:", error);
-        res.status(500).json({ message: "Internal server error", error: error.message });
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "Business ID is required" });
     }
-}
+
+    const deletedBusiness = await SavedBusinessModel.findByIdAndDelete(id);
+    if (!deletedBusiness) {
+      return res.status(404).json({ message: "Saved business not found" });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: "Saved business deleted successfully",
+      data: deletedBusiness,
+    });
+  } catch (error) {
+    console.error("Error in deleteSavedBusinessById:", error);
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
