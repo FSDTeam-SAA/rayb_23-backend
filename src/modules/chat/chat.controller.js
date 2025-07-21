330a3fca45187031cd67b44b9dd2980ab301c5e1
Sajjad177 <sajjadhossainx06@gmail.com>
2025-07-21 16:03:52 +0600

    business man chat get successfully

--- src/modules/chat/chat.controller.js
+++ src/modules/chat/chat.controller.js
@@ -52,10 +52,29 @@ const getMyChat = async (req, res) => {
   }
 };
 
+const getChatForBusinessMan = async (req, res) => {
+  try {
+    const { businessId } = req.params;
+    const result = await chatService.getChatForBusinessMan(businessId);
+
+    return res.status(200).json({
+      success: true,
+      code: 200,
+      message: "Your chat get successfully",
+      data: result,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ success: false, code: 400, message: error.message });
+  }
+};
+
 const chatController = {
   createChat,
   getChat,
   getMyChat,
+  getChatForBusinessMan,
 };
 
 module.exports = chatController;
--- src/modules/chat/chat.router.js
+++ src/modules/chat/chat.router.js
@@ -3,20 +3,13 @@ const chatController = require("./chat.controller");
 
 const router = Router();
 
-router.post(
-  "/create",
-  chatController.createChat
-);
+router.post("/create", chatController.createChat);
 
-router.get(
-  "/",
-  chatController.getChat
-);
+router.get("/", chatController.getChat);
 
-router.get(
-  "/my-chat/:userId",
-  chatController.getMyChat
-);
+router.get("/my-chat/:userId", chatController.getMyChat);
+
+router.get("/business-chat/:businessId", chatController.getChatForBusinessMan);
 
 const chatRouter = router;
 module.exports = chatRouter;
