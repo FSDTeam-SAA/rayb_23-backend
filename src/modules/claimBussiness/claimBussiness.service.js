666ea9cb154cff3173b206e5bf0c2ba24533bacd
Ahmedturaj <ahmedturaj12@gmail.com>
2025-08-27 15:13:08 +0600

    add Id in claim business

--- src/modules/claimBussiness/claimBussiness.service.js
+++ src/modules/claimBussiness/claimBussiness.service.js
@@ -59,6 +59,9 @@ const getAllClaimBussiness = async ({ claimType, time, sortBy }) => {
   if (claimType && ["pending", "approved", "rejected"].includes(claimType)) {
     filter.status = claimType;
   }
+  if (id) {
+    filter._id = id;
+  }
 
   if (time && ["last-7", "last-30"].includes(time)) {
     const now = new Date();
